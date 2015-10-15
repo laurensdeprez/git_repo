14796e4a054322017c95e30a4a65d1b8e1777758
mlaleman <michiellaleman@gmail.com>
2015-10-15 12:14:01 +0200

    Added reading of floats.

---
+++ read_rand.py
@@ -0,0 +1,11 @@
+from __future__ import print_function, division
+import numpy as np
+import sys
+
+N = int(sys.argv[1])
+
+print("N is", N)
+
+for line in sys.stdin:
+	temp = float(line)
+	print(temp)
